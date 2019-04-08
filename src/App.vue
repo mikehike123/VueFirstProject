6b79b41d5cd904f949d9a70b5136ae3c2f020169
mike <mikehike123@gmail.com>
2019-04-07 20:36:36 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <MyHeader />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App Lets change this!!!"/>
+    <MyForm />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyForm from './components/MyForm.vue'
+import MyHeader from './components/MyHeader.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    MyForm,
+    MyHeader,
+  },
+  mounted () {
+    axios
+      .get('http://localhost/shoppingList/list/read.php')
+      .then(response => {
+          this.listArray = response.data['records'];
+          alert(JSON.stringify(this.listArray))
+       })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+body
+{
+  margin:0px;
+}
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="top-header">
+        <h1>My List</h1>
+    </div>  
+</template>
+<script>
+export default {
+    name:"MyHeader"
+    
+}
+</script>
+<style scoped>
+    #top-header
+    {
+        width:100%;
+        background-color: darkslategray;
+        padding:20px;
+    }
+    h1
+    {
+        margin:auto;
+        color:white;
+    }
+</style>
+
